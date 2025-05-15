c6cb05f2b68abb2677a7d39a67396a56571457ce
santosh methre <santoshmethre22@gmail.com>
2025-05-15 19:46:32 +0530

    contest biweekyly 156

--- Contest/Biweekly/156/Minimum Operations to Convert All Elements to Zero.cpp
+++ Contest/Biweekly/156/Minimum Operations to Convert All Elements to Zero.cpp
@@ -4,34 +4,10 @@
 #include<unordered_map>
 using namespace std;
 
-class Solution {
-public:
-    int maxFreqSum(string s) {
-
-        unordered_map<char,int>mp;
-
-    for(auto i:s){
-        mp[i]++;
-
-    }
-        int ovel=0;
-        int consonent=0;
-        for(auto i:mp){
-            if(i.first=='a'||i.first=='e'||i.first=='i'||i.first=='o'||i.first=='u'){
-
-                ovel=max(ovel,i.second);
-
-            }
-            else{
-                consonent=max(consonent,i.second);
-            }
-        }
 
 
-        return consonent+ovel;
-        
-    }
-};
+
+
 
 int main() {
 
